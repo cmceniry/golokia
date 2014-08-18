397a97e79a147415115176a668ec73fe7caf5aef
Chris McEniry <cmceniry@corgalabs.com>
2014-08-18 10:17:36 -0700

    * Jolokia Client for connection reuse * test cleanup * go fmt

--- golokia.go
+++ golokia.go
@@ -2,8 +2,9 @@ package golokia
 
 import (
 	"encoding/json"
-	"net/http"
 	"errors"
+	"fmt"
+	"net/http"
 	"sort"
 )
 
@@ -12,6 +13,7 @@ type JolokiaResponse struct {
 	Timestamp uint32
 	Request   map[string]interface{}
 	Value     map[string]interface{}
+	Error     string
 }
 
 type JolokiaReadResponse struct {
@@ -19,6 +21,12 @@ type JolokiaReadResponse struct {
 	Timestamp uint32
 	Request   map[string]interface{}
 	Value     interface{}
+	Error     string
+}
+
+type Client struct {
+	Service    string
+	HttpClient *http.Client
 }
 
 func get(url string) (*JolokiaResponse, error) {
@@ -26,7 +34,7 @@ func get(url string) (*JolokiaResponse, error) {
 	if respErr != nil {
 		return nil, respErr
 	}
-        defer resp.Body.Close()
+	defer resp.Body.Close()
 	var respJ JolokiaResponse
 	dec := json.NewDecoder(resp.Body)
 	if err := dec.Decode(&respJ); err != nil {
@@ -40,7 +48,7 @@ func getAttr(url string) (*JolokiaReadResponse, error) {
 	if respErr != nil {
 		return nil, respErr
 	}
-        defer resp.Body.Close()
+	defer resp.Body.Close()
 	var respJ JolokiaReadResponse
 	dec := json.NewDecoder(resp.Body)
 	if err := dec.Decode(&respJ); err != nil {
@@ -94,6 +102,98 @@ func ListProperties(service, domain, bean string) ([]string, error) {
 
 func GetAttr(service, domain, bean, attr string) (interface{}, error) {
 	resp, err := getAttr(service + "/jolokia/read/" + domain + ":" + bean + "/" + attr)
+	if err != nil {
+		return "", err
+	}
+	return resp.Value, nil
+}
+
+func NewClient(host, port string) *Client {
+	return &Client{
+		"http://" + host + ":" + port,
+		&http.Client{},
+	}
+}
+
+func (c *Client) get(uripath string) (*JolokiaResponse, error) {
+	resp, respErr := c.HttpClient.Get(c.Service + uripath)
+	if respErr != nil {
+		return nil, respErr
+	}
+	defer resp.Body.Close()
+	var respJ JolokiaResponse
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&respJ); err != nil {
+		return nil, err
+	}
+	if respJ.Status != 200 {
+		return nil, fmt.Errorf("golokia: bad get response: %s", respJ.Error)
+	}
+	return &respJ, nil
+}
+
+func (c *Client) getAttr(uripath string) (*JolokiaReadResponse, error) {
+	resp, respErr := c.HttpClient.Get(c.Service + uripath)
+	if respErr != nil {
+		return nil, respErr
+	}
+	defer resp.Body.Close()
+	var respJ JolokiaReadResponse
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&respJ); err != nil {
+		return nil, err
+	}
+	if respJ.Status != 200 {
+		return nil, fmt.Errorf("golokia: bad get response: %s", respJ.Error)
+	}
+	return &respJ, nil
+}
+
+func (c *Client) ListDomains() ([]string, error) {
+	resp, err := c.get("/jolokia/list?maxDepth=1")
+	if err != nil {
+		return nil, err
+	}
+	ret := make([]string, 0, len(resp.Value))
+	for key, _ := range resp.Value {
+		ret = append(ret, key)
+	}
+	sort.Strings(ret)
+	return ret, nil
+}
+
+func (c *Client) ListBeans(domain string) ([]string, error) {
+	resp, err := c.get("/jolokia/list/" + domain + "?maxDepth=1")
+	if err != nil {
+		return nil, err
+	}
+	ret := make([]string, 0, len(resp.Value))
+	for key, _ := range resp.Value {
+		ret = append(ret, key)
+	}
+	sort.Strings(ret)
+	return ret, nil
+}
+
+func (c *Client) ListProperties(domain, bean string) ([]string, error) {
+	resp, err := c.get("/jolokia/list/" + domain + "/" + bean + "?maxDepth=2")
+	if err != nil {
+		return nil, err
+	}
+	if _, ok := resp.Value["attr"]; !ok {
+		return nil, errors.New("Invalid repsonse format - missing attr")
+	}
+	respItems := resp.Value["attr"].(map[string]interface{})
+	ret := make([]string, 0, len(respItems))
+	for key, _ := range respItems {
+		ret = append(ret, key)
+	}
+	sort.Strings(ret)
+	return ret, nil
+}
+
+func (c *Client) GetAttr(domain, bean, attr string) (interface{}, error) {
+	resp, err := c.getAttr("/jolokia/read" + domain + ":" + bean + "/" + attr)
 	if err != nil {
 		return "", err
 	}
--- golokia_test.go
+++ golokia_test.go
@@ -16,7 +16,7 @@ func TestListDomains(t *testing.T) {
 }
 
 func TestListBeans(t *testing.T) {
-	beans, err := ListBeans("http://localhost:7025","java.lang")
+	beans, err := ListBeans("http://localhost:7025", "java.lang")
 	if err != nil {
 		t.Errorf("err(%s) returned, err")
 	}
@@ -26,7 +26,7 @@ func TestListBeans(t *testing.T) {
 }
 
 func TestListProperties(t *testing.T) {
-	props, err := ListProperties("http://localhost:7025","java.lang","type=Threading")
+	props, err := ListProperties("http://localhost:7025", "java.lang", "type=Threading")
 	if err != nil {
 		t.Errorf("err(%s), returned, err")
 	}
@@ -35,12 +35,58 @@ func TestListProperties(t *testing.T) {
 	}
 }
 
-func TestGetAttr(t *testing.T)  {
-	val, err := GetAttr("http://localhost:7025","java.lang","type=Threading","PeakThreadCount")
+func TestGetAttr(t *testing.T) {
+	val, err := GetAttr("http://localhost:7025", "java.lang", "type=Threading", "PeakThreadCount")
 	if err != nil {
 		t.Errorf("err(%s), returned", err)
 	}
 	if val.(float64) <= 100.0 {
 		t.Errorf("GetAttr(PeakThreadCount) = %v, want > 100", val)
+	}
+}
+
+func TestClientListDomains(t *testing.T) {
+	client := NewClient("localhost", "7025")
+	domains, err := client.ListDomains()
+	if err != nil {
+		t.Errorf("err(%s) returned", err)
+	}
+	if len(domains) != 11 {
+		t.Errorf("ListDomains = %v, want %v : %#v", len(domains), 11, domains)
+	}
+}
+
+func TestClientListBeans(t *testing.T) {
+	client := NewClient("localhost", "7025")
+	beans, err := client.ListBeans("java.lang")
+	if err != nil {
+		t.Errorf("err(%s) returned", err)
+	}
+	if len(beans) != 14 {
+		t.Errorf("ListBeans(java.lang) = %v, want %v : %#v", len(beans), 14, beans)
+	}
+}
+
+func TestClientListProperties(t *testing.T) {
+	client := NewClient("localhost", "7025")
+	props, err := client.ListProperties("java.lang", "type=Threading")
+	if err != nil {
+		t.Errorf("err(%s), returned", err)
+	}
+	if len(props) != 16 {
+		t.Errorf("ListProperties(type=Threading) = %v, want %v, : %#v", len(props), 16, props)
+	}
+}
+
+func TestClientGetAttr(t *testing.T) {
+	client := NewClient("localhost", "7025")
+	val, err := client.GetAttr("java.lang", "type=Threading", "PeakThreadCount")
+	if err != nil {
+		t.Errorf("err(%s), returned", err)
+		return
+	}
+	if val.(float64) <= 100.0 {
+		t.Errorf("GetAttr(PeakThreadCount) = %v, want > 100", val)
+		return
 	}
 }
